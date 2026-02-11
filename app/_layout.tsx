3289a5aa1b2feee1f4c30d16d2b2ba4ecce40796
toki <clevertrever31@gmail.com>
2026-02-11 15:25:58 -0600

    Added header titles for login, signup, and homepage. Then turned off (auth) header.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerBackVisible: false }} />
+      <Stack.Screen name="index" options={{ headerBackVisible: false, title: "Home" }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
   );
 }
